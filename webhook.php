3d0f9eab5d5876da54baf08e77f3cfe8ca191efa
girogiacomo <39419444+girogiacomo@users.noreply.github.com>
2026-05-24 19:37:48 +0200

    Customized webhook for easier dockerized deployment

--- webhook.php
+++ webhook.php
@@ -15,32 +15,34 @@
 **/
 
 // Set Variables
-$LOCAL_ROOT         = "/var/www/html_mene";
+$LOCAL_ROOT         = "/var/www/html";
 $LOCAL_REPO_NAME    = "mattiameneghin.tk";
 $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+// $LOCAL_LOG          =  "{$LOCAL_ROOT}/log.txt"
+$LOCAL_LOG          =  "/log_updater.txt"
 $REMOTE_REPO        = "git@github.com:Mattiamene1/mattiameneghin.tk.git";
 $REMOTE_REPO_URL    = "https://github.com/Mattiamene1/mattiameneghin.tk.git";
 $BRANCH             = "main";
 
 $date = date('Y/m/d H:i:s', time());
-shell_exec("echo \"$date - PHP Start HERE\" >> " . $LOCAL_ROOT . "/log.txt");
-shell_exec("echo \"         Local repo: $LOCAL_REPO \" >> " . $LOCAL_ROOT . "/log.txt");
-shell_exec("echo \"         Is_dir: " . var_export(is_dir($LOCAL_REPO), true) . " \" >> " . $LOCAL_ROOT . "/log.txt");
-shell_exec("echo \"         File_exists: " . var_export(file_exists($LOCAL_REPO), true) . " \" >> " . $LOCAL_ROOT . "/log.txt");
+shell_exec("echo \"$date - PHP Start HERE\" >> " . $LOCAL_LOG);
+shell_exec("echo \"         Local repo: $LOCAL_REPO \" >> " . $LOCAL_LOG);
+shell_exec("echo \"         Is_dir: " . var_export(is_dir($LOCAL_REPO), true) . " \" >> " . $LOCAL_LOG);
+shell_exec("echo \"         File_exists: " . var_export(file_exists($LOCAL_REPO), true) . " \" >> " . $LOCAL_LOG);
 
 if ( $_POST['payload'] ) {
   // Only respond to POST requests from Github
 
   if( file_exists($LOCAL_REPO) ){
 
-    shell_exec("echo \"         GIT PULL\" >> " . $LOCAL_ROOT . "/log.txt");
+    shell_exec("echo \"         GIT PULL\" >> " . $LOCAL_LOG);
     // If there is already a repo, just run a git pull to grab the latest changes
     shell_exec("cd {$LOCAL_REPO} && git pull");
 
     // die("done " . mktime());
   } else {
 
-    shell_exec("echo \"         GIT CLONE\" >> " . $LOCAL_ROOT . "/log.txt");
+    shell_exec("echo \"         GIT CLONE\" >> " . $LOCAL_LOG);
     // If the repo does not exist, then clone it into the parent directory
     shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO_URL}");
 
@@ -48,6 +50,6 @@ if ( $_POST['payload'] ) {
   }
 }
 
-shell_exec("echo \"PHP END\" >> " . $LOCAL_ROOT . "/log.txt");
-shell_exec("echo \" ---------------------------------------- \" >> " . $LOCAL_ROOT . "/log.txt");
+shell_exec("echo \"PHP END\" >> " . $LOCAL_LOG);
+shell_exec("echo \" ---------------------------------------- \" >> " . $LOCAL_LOG);
 ?>
